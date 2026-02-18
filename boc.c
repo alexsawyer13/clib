1dd6d84be1ace9577463fb738b1aed1a6a70aabf
Alex <alex.sawyer03@outlook.com>
2026-02-18 21:28:59 +0000

    Restructured folders

--- boc.c
+++ boc.c
@@ -2,8 +2,8 @@
 
 int boc_main(boc *b)
 {
-    boc_add_exec("test");
-    boc_add_src("test.c");
-	boc_add_src("clib.c");
+    boc_add_exec("build/test");
+    boc_add_src("src/test.c");
+	boc_add_src("src/clib.c");
     return 0;
 }
